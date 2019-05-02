c98ffd6d8b93c465bc770346b2d4039948898edb
Infinical <inficalvin@gmail.com>
2019-05-02 10:44:25 +0300

    create components and install router

---
+++ src/components/Categories.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Meals from "./Meals";
+export default class Categories extends Component {
+  render() {
+    return (
+      <div>
+        <Meals />
+      </div>
+    );
+  }
+}
---
+++ src/components/Meals.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Meals extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Meals</h3>
+      </div>
+    );
+  }
+}
