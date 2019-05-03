ed7e63552d0fd769e617c0184b3ec952a9d660b6
Infinical <inficalvin@gmail.com>
2019-05-03 15:26:05 +0300

    Style sign and sign up

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button } from "./Button";
+import styled from "styled-components";
 import iziToast from "izitoast";
 
 export default class Signup extends Component {
@@ -81,32 +81,84 @@ export default class Signup extends Component {
     const { Email, Password, Passwordc } = this.state;
     return (
       <React.Fragment>
-        <header as="h1" textAlign="center" className="signup-h1">
-          Sign Up
-        </header>
-        <form>
-          <input
-            type="email"
-            label="Email"
-            placeholder="email@email.com"
-            value={Email}
-            onChange={this.onChangeEmail}
-          />
-          <input
-            type="password"
-            label="Password"
-            onChange={this.onChangePassword}
-            value={Password}
-          />
-          <input
-            type="password"
-            label="Confirm Password"
-            value={Passwordc}
-            onChange={this.onChangePasswordc}
-          />
-          <Button label="Sign Up" onClick={this.onSignUp} />
-        </form>
+        <div className="container">
+          <div className="row">
+            <h1 className="h3 mb-3 font-weight-normal text-capitalize">
+              Sign Up
+            </h1>
+            <Form className="form-signup">
+              <input
+                className="form-control"
+                type="email"
+                label="Email"
+                placeholder="email@email.com"
+                value={Email}
+                onChange={this.onChangeEmail}
+                required
+                autoFocus
+              />
+              <input
+                className="form-control"
+                type="password"
+                label="Password"
+                onChange={this.onChangePassword}
+                value={Password}
+                required
+              />
+              <input
+                className="form-control"
+                type="password"
+                label="Confirm Password"
+                value={Passwordc}
+                onChange={this.onChangePasswordc}
+                required
+              />
+              <button
+                class="btn btn-m btn-primary btn-block"
+                type="submit"
+                onClick={this.onSignUp}
+              >
+                Sign Up
+              </button>
+            </Form>
+          </div>
+        </div>
       </React.Fragment>
     );
   }
 }
+
+const Form = styled.form`
+  height: 100%;
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-align: center;
+  align-items: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background: var(--mainWhite);
+
+  .form-signup {
+    width: 100%;
+    max-width: 330px;
+    padding: 15px;
+    margin: auto;
+  }
+  .form-signup input[type="email"] {
+    margin-bottom: -1px;
+    border-bottom-right-radius: 0;
+    border-bottom-left-radius: 0;
+  }
+  .form-signup input[type="password"] {
+    margin-bottom: 10px;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+  }
+  .form-signin .form-control {
+    position: relative;
+    box-sizing: border-box;
+    height: auto;
+    padding: 10px;
+    font-size: 16px;
+  }
+`;
