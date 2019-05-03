a84393ffe8e293bd7bc4ac97bd361ed9ca61e424
Infinical <inficalvin@gmail.com>
2019-05-03 15:38:24 +0300

    Cart (#5)
    * Initial circleci setup
    
    * Circle ci config
    
    * Increase or decrease items in cart
    
    * Setup 404 page
    
    * Style sign and sign up

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { Button } from "./Button";
 import iziToast from "izitoast";
+import styled from "styled-components";
 
 export default class Signin extends Component {
   constructor(props) {
@@ -66,26 +66,79 @@ export default class Signin extends Component {
     const { Email, Password } = this.state;
     return (
       <React.Fragment>
-        <header as="h1" textAlign="center" className="signin-h1">
-          Sign In
-        </header>
-        <form>
-          <input
-            type="email"
-            label="Email"
-            placeholder="email@email.com"
-            value={Email}
-            onChange={this.onChangeEmail}
-          />
-          <input
-            type="password"
-            label="Password"
-            onChange={this.onChangePassword}
-            value={Password}
-          />
-          <Button label="Sign In" onClick={this.onSignIn} />
-        </form>
+        <div className="container">
+          <div className="row">
+            <h1 className="h3 mb-3 font-weight-normal text-capitalize">
+              Sign In
+            </h1>
+            <Form className="form-signin">
+              <label>Email Address</label>
+
+              <input
+                className="form-control"
+                type="email"
+                label="Email"
+                placeholder="Email Address"
+                value={Email}
+                onChange={this.onChangeEmail}
+                required
+                autoFocus
+              />
+              <label>Password</label>
+              <input
+                className="form-control"
+                type="password"
+                label="Password"
+                onChange={this.onChangePassword}
+                value={Password}
+                placeholder="Password"
+                required
+              />
+              <button
+                class="btn btn-m btn-primary btn-block"
+                type="submit"
+                onClick={this.onSignIn}
+              >
+                Sign In
+              </button>
+            </Form>
+          </div>
+        </div>
       </React.Fragment>
     );
   }
 }
+const Form = styled.form`
+  height: 100%;
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-align: center;
+  align-items: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background: var(--mainWhite);
+
+  .form-signin {
+    width: 100%;
+    max-width: 330px;
+    padding: 15px;
+    margin: auto;
+  }
+  .form-signin input[type="email"] {
+    margin-bottom: -1px;
+    border-bottom-right-radius: 0;
+    border-bottom-left-radius: 0;
+  }
+  .form-signin input[type="password"] {
+    margin-bottom: 10px;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+  }
+  .form-signin .form-control {
+    position: relative;
+    box-sizing: border-box;
+    height: auto;
+    padding: 10px;
+    font-size: 16px;
+  }
+`;
--- src/context.js
+++ src/context.js
@@ -7,9 +7,9 @@ class MealsProvider extends Component {
   state = {
     meals: [],
     mealDetail: mealDetail,
-    cart: foodCategories,
-    cartSubTotal: 20,
-    cartTax: 10,
+    cart: [],
+    cartSubTotal: 0,
+    cartTax: 0,
     cartTotal: 0
   };
 
@@ -52,24 +52,101 @@ class MealsProvider extends Component {
         return { meals: tempMeal, cart: [...this.state.cart, meal] };
       },
       () => {
-        console.log(this.state);
+        this.addTotals();
       }
     );
   };
 
   increment = id => {
-    console.log("Increment");
+    let tempCart = [...this.state.cart];
+    const selectedMeal = tempCart.find(item => item.id === id);
+    const index = tempCart.indexOf(selectedMeal);
+    const meal = tempCart[index];
+
+    meal.count = meal.count + 1;
+    meal.total = meal.count * meal.price;
+
+    this.setState(
+      () => {
+        return { cart: [...tempCart] };
+      },
+      () => {
+        this.addTotals();
+      }
+    );
   };
   decrement = id => {
-    console.log("Decrement");
+    let tempCart = [...this.state.cart];
+    const selectedMeal = tempCart.find(item => item.id === id);
+    const index = tempCart.indexOf(selectedMeal);
+    const meal = tempCart[index];
+
+    meal.count = meal.count - 1;
+    if (meal.count === 0) {
+      this.removeItem(id);
+    } else {
+      meal.total = meal.count * meal.price;
+      this.setState(
+        () => {
+          return { cart: [...tempCart] };
+        },
+        () => {
+          this.addTotals();
+        }
+      );
+    }
   };
 
   removeItem = id => {
-    console.log("Remove item");
+    let tempMeal = [...this.state.meals];
+    let tempCart = [...this.state.cart];
+
+    tempCart = tempCart.filter(item => item.id !== id);
+
+    const index = tempMeal.indexOf(this.getMeal(id));
+    let removedMeal = tempMeal[index];
+    removedMeal.inCart = false;
+    removedMeal.count = 0;
+    removedMeal.total = 0;
+
+    this.setState(
+      () => {
+        return {
+          cart: [...tempCart],
+          meals: [...tempMeal]
+        };
+      },
+      () => {
+        this.addTotals();
+      }
+    );
   };
 
   clearCart = id => {
-    console.log("Cleared cart");
+    this.setState(
+      () => {
+        return { cart: [] };
+      },
+      () => {
+        this.setMeals();
+        this.addTotals();
+      }
+    );
+  };
+
+  addTotals = () => {
+    let subTotal = 0;
+    this.state.cart.map(item => (subTotal += item.total));
+    const tempTax = subTotal * 0.1;
+    const tax = parseFloat(tempTax.toFixed(2));
+    const total = subTotal + tax;
+    this.setState(() => {
+      return {
+        cartSubTotal: subTotal,
+        cartTax: tax,
+        cartTotal: total
+      };
+    });
   };
 
   render() {
