129d66a8784c7f08e50e391d295fe027920a8f10
Infinical <inficalvin@gmail.com>
2019-05-03 14:37:18 +0300

    Increase or decrease items in cart

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function CartTotals(value) {
+export default function CartTotals({ value }) {
   const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
   return (
     <React.Fragment>
